6688b4eb3c5341b0b9321fbb85b468297a40936b
Huynh Thi Thao Minh <huynhthithaominhh0802@gmail.com>
2025-12-19 07:51:19 +0700

    US-11 AC01: them mon thanh cong

---
+++ them_mon.py
@@ -0,0 +1,10 @@
+menu = []
+
+def them_mon(name, price, category):
+    item = {
+        "name": name,
+        "price": price,
+        "category": category
+    }
+    menu.append(item)
+    return "Thêm món thành công"
